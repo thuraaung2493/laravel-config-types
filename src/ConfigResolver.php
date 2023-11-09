cd8d8578f56170a834d4874d17eb6eb76fbba12b
thuraaung2493 <thuraaung2493@gmail.com>
2023-11-09 16:14:54 +0630

    Set initial key

--- src/ConfigResolver.php
+++ src/ConfigResolver.php
@@ -7,17 +7,31 @@ namespace Thuraaung\ConfigTypes;
 use Illuminate\Contracts\Config\Repository;
 use Illuminate\Support\Collection;
 
-final readonly class ConfigResolver
+final class ConfigResolver
 {
+    private ?string $initKey = null;
+
     public function __construct(
-        private Repository $config
+        private readonly Repository $config
     ) {
+    }
+
+    public function setInitKey(string $initKey): self
+    {
+        $this->initKey = $initKey;
+
+        return $this;
+    }
+
+    private function resolveKey(string $key): string
+    {
+        return \is_null($this->initKey) ? $key : "{$this->initKey}.{$key}";
     }
 
     public function string(string $key, string|null $default = null): string
     {
         return (string) $this->config->get(
-            key: $key,
+            key: $this->resolveKey($key),
             default: $default,
         );
     }
@@ -25,7 +39,7 @@ final readonly class ConfigResolver
     public function number(string $key, int|null $default = null): int
     {
         return (int) $this->config->get(
-            key: $key,
+            key: $this->resolveKey($key),
             default: $default,
         );
     }
@@ -33,7 +47,7 @@ final readonly class ConfigResolver
     public function float(string $key, float|null $default = null): float
     {
         return (float) $this->config->get(
-            key: $key,
+            key: $this->resolveKey($key),
             default: $default,
         );
     }
@@ -41,7 +55,7 @@ final readonly class ConfigResolver
     public function boolean(string $key, bool|null $default = null): bool
     {
         return (bool) $this->config->get(
-            key: $key,
+            key: $this->resolveKey($key),
             default: $default,
         );
     }
@@ -49,7 +63,7 @@ final readonly class ConfigResolver
     public function array(string $key, array|null $default = null): array
     {
         return (array) $this->config->get(
-            key: $key,
+            key: $this->resolveKey($key),
             default: $default,
         );
     }
@@ -58,7 +72,7 @@ final readonly class ConfigResolver
     {
         return Collection::make(
             items: (array) $this->config->get(
-                key: $key,
+                key: $this->resolveKey($key),
                 default: $default,
             ),
         );
